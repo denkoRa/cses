fd3b86a459f70e4dfaa7e94b4e598e1fcfb1c922
Radenko Pavlovic <radenko.pavlovic@nutanix.com>
2020-05-31 23:37:44 +0200

    Started work on range queries.

---
+++ range_queries/range_sum_queries_I.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+
+#define endl '\n'
+#define sync_cin                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define sz(a) (int)a.size()
+#define all(a) a.begin(), a.end()
+#define TYPEMAX(type)   std::numeric_limits<type>::max()
+#define TYPEMIN(type)   std::numeric_limits<type>::min()
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+//-----------------------------------------------------------------------------
+
+template<typename T>
+inline istream& operator>>(istream& is, vector<T>& v) {
+  for (int ii = 0; ii < sz(v); ++ii) {
+    is >> v[ii];
+  }
+  return is;
+}
+
+template<typename T1, typename T2>
+inline istream& operator>>(istream& is, pair<T1, T2>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template<typename T>
+inline ostream& operator<<(ostream& os, vector<vector<T>>& mat) {
+  for (int ii = 0; ii < sz(mat); ++ii) {
+    for (int jj = 0; jj < sz(mat[0]); ++jj) {
+      os << mat[ii][jj] << " ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+//-----------------------------------------------------------------------------
+
+int main() {
+  sync_cin;
+
+  int n, q; cin >> n >> q;
+  vector<int> x(n); cin >> x;
+
+  vector<ll> prefix_sum(n);
+  for (int ii = 0; ii < n; ++ii) {
+    prefix_sum[ii] = ii == 0 ? x[ii] : prefix_sum[ii - 1] + x[ii];
+  }
+
+  for (int ii = 0; ii < q; ++ii) {
+    int a, b; cin >> a >> b;
+    --a; --b;
+    ll result = prefix_sum[b] - (a > 0 ? prefix_sum[a - 1] : 0);
+    cout << result << endl;
+  }
+
+  return 0;
+}
